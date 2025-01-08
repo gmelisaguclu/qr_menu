59b9da2c2772267133d2885bc5a2aeef2ff10399
gclmls <gmelisaguclu@gmail.com>
2025-01-08 16:31:30 +0300

    regulation:mymenu

--- app/mymenu/page.tsx
+++ app/mymenu/page.tsx
@@ -1,19 +1,39 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React from "react";
+import Link from "next/link";
 
 function page() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <div className="border-4 flex flex-col items-center mt-4 rounded-3xl h-80">
-        <div className="flex items-center justify-center mt-9 mx-6 font-hand text-xl">
-          Kendi mutfağınızın yıldızı olun, menünüzü özgürce tasarlayın.
-        </div>
-
-        <div className="flex mt-10 gap-3">
-          <div className="font-hand text-lg">Menü Oluştur</div>
-          <Plus />
-        </div>
-      </div>
+      <Card className="w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[40%] aspect-[4/3] flex items-center flex-col justify-center">
+        <CardHeader className="gap-5">
+          <CardTitle className="text-3xl text-center tracking-wide ">
+            Menünüzü Oluşturun
+          </CardTitle>
+          <CardDescription className="text-xl text-center ">
+            Kendi mutfağınızın yıldızı olun, menünüzü özgürce tasarlayın.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex my-4 justify-center">
+          <div>
+            <Link href={"/menu-olustur"}>
+              <Button>
+                Menü Oluştur <Plus />
+              </Button>
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
